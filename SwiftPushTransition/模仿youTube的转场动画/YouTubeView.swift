fb4de3ed7eaab854f8ceb03fb17f3130acd5a21c
zhangmingwei <zjjzmw1@163.com>
2018-03-01 15:55:53 +0800

    添加带tabbar的情况

--- SwiftPushTransition/模仿youTube的转场动画/YouTubeView.swift
+++ SwiftPushTransition/模仿youTube的转场动画/YouTubeView.swift
@@ -8,22 +8,42 @@
 
 import UIKit
 
+/// iPhoneX
+let kYoutube_is_iphoneX     =   UIScreen.main.bounds.height == 812
+
+/// tabbar的高度 49
+var kYoutube_tabbar_height: CGFloat {
+    get {
+        if kYoutube_is_iphoneX {
+            return 49.0 + 34.0
+        } else {
+            return 49.0
+        }
+    }
+}
+
+/// 屏幕宽度
 let kYoutubeScreenWidth         = UIScreen.main.bounds.width
+/// 屏幕高度
 let kYoutubeScreenHeight        = UIScreen.main.bounds.height
+/// youtube最小的宽度
+let kYouTuBeMinWidth: CGFloat    =   (kYoutubeScreenWidth - 60) / 2.0
+/// youtube最小的高度
+let kYouTuBeMinHeight: CGFloat    = ((kYoutubeScreenWidth - 60) / 2.0) * 0.66
+/// 小窗口到屏幕的间隙
+let kYouTuBeSpacingRight: CGFloat = 10.0
+/// 有tabbar的时候下面空出tabbar的高度 --- 没有tabbar的时候，把下面这句改下就OK了。
+let kYouTuBeSpacingBottom: CGFloat = 10.0 + kYoutube_tabbar_height
 
 class YouTubeView: UIView {
 
     /// 单利
 //    let shareYouTubeView: YouTubeDetailView
-    /// youtube最小的宽度
-    let youTuBeMinWidth: CGFloat    =   (kYoutubeScreenWidth - 60) / 2.0
-    /// youtube最小的高度
-    let youTuBeMinHeight: CGFloat    = ((kYoutubeScreenWidth - 60) / 2.0) * 0.66
     /// 顶部播放视频的view
     var topPlayerView: UIView!
     /// 视频控件当前是小窗口吗
     var isCurrentPlayerSmall: Bool = false
-    /// 底部的view
+    /// 底部的view -------- 在这上面添加自己的view
     var bottomView: UIView!
     
     override init(frame: CGRect) {
@@ -63,34 +83,34 @@ class YouTubeView: UIView {
         } else if gestureRecognizer.state == UIGestureRecognizerState.changed { // 正在滑动
             var w = kYoutubeScreenWidth * (1-progress)
             if self.isCurrentPlayerSmall { // 当前是小窗口的话
-                w = youTuBeMinWidth - kYoutubeScreenWidth*progress
+                w = kYouTuBeMinWidth - kYoutubeScreenWidth*progress
             }
             var top = topPlayerView.frame.origin.y * (1+progress)
-            if w < youTuBeMinWidth { // 设置宽度的最小值
-                w = youTuBeMinWidth
+            if w < kYouTuBeMinWidth { // 设置宽度的最小值
+                w = kYouTuBeMinWidth
             }
             if w > kYoutubeScreenWidth {
                 w = kYoutubeScreenWidth
             }
-            if top > kYoutubeScreenHeight - self.youTuBeMinHeight - 10 {
-                top = kYoutubeScreenHeight - self.youTuBeMinHeight - 10
+            if top > kYoutubeScreenHeight - kYouTuBeMinHeight - kYouTuBeSpacingBottom {
+                top = kYoutubeScreenHeight - kYouTuBeMinHeight - kYouTuBeSpacingBottom
             }
             if top < 0 {
                 top = 0
             }
-            var x = kYoutubeScreenWidth - w - 10*progress*1.2
-            var y = kYoutubeScreenHeight - self.youTuBeMinHeight - 10 + kYoutubeScreenHeight*progress
+            var x = kYoutubeScreenWidth - w - kYouTuBeSpacingRight*progress*1.2
+            var y = kYoutubeScreenHeight - kYouTuBeMinHeight - kYouTuBeSpacingBottom + kYoutubeScreenHeight*progress
             if !self.isCurrentPlayerSmall {
                 y = kYoutubeScreenHeight*progress
             }
             if y < 0 {
                 y = 0
             }
-            if y > kYoutubeScreenHeight - self.youTuBeMinHeight - 10 {
-                y = kYoutubeScreenHeight - self.youTuBeMinHeight - 10
+            if y > kYoutubeScreenHeight - kYouTuBeMinHeight - kYouTuBeSpacingBottom {
+                y = kYoutubeScreenHeight - kYouTuBeMinHeight - kYouTuBeSpacingBottom
             }
             if self.isCurrentPlayerSmall { // 当前是小窗口的话
-                x = kYoutubeScreenWidth - w - 10*(1+progress*2)
+                x = kYoutubeScreenWidth - w - kYouTuBeSpacingRight*(1+progress*2)
                 self.frame = CGRect(x:x, y:y, width:w, height: w * 0.66)
                 topPlayerView?.frame = CGRect.init(x: 0, y: 0, width: w, height: w * 0.66)
             } else {
@@ -102,7 +122,7 @@ class YouTubeView: UIView {
             // 更新底部view的透明度
             if progress > 0 {
                 bottomView.alpha = 1 - progress*2 < 0 ? 0 : 1 - progress*2
-                if self.frame.origin.y + topPlayerView.frame.height > kYoutubeScreenHeight - 100 { // 避免在最底部的时候下拉就展示
+                if self.frame.origin.y + topPlayerView.frame.height > kYoutubeScreenHeight - 111 { // 避免在最底部的时候下拉就展示
                     bottomView.alpha = 0
                 }
             } else {
@@ -125,8 +145,8 @@ class YouTubeView: UIView {
                 })
             } else { // 变成小窗口
                 UIView.animate(withDuration: 0.2, animations: {
-                    self.frame = CGRect(x:kYoutubeScreenWidth - self.youTuBeMinWidth - 10, y:kYoutubeScreenHeight - self.youTuBeMinHeight - 10, width:self.youTuBeMinWidth , height: self.youTuBeMinHeight)
-                    self.topPlayerView.frame = CGRect.init(x: 0, y: 0, width: self.youTuBeMinWidth, height: self.youTuBeMinHeight)
+                    self.frame = CGRect(x:kYoutubeScreenWidth - kYouTuBeMinWidth - kYouTuBeSpacingRight, y:kYoutubeScreenHeight - kYouTuBeMinHeight - kYouTuBeSpacingBottom , width:kYouTuBeMinWidth , height: kYouTuBeMinHeight)
+                    self.topPlayerView.frame = CGRect.init(x: 0, y: 0, width: kYouTuBeMinWidth, height: kYouTuBeMinHeight)
                     // 更新底部view的位置
                     self.bottomView.frame =  CGRect.init(x: 0, y: self.topPlayerView.frame.origin.y + self.topPlayerView.frame.height, width: self.topPlayerView.frame.width, height: kYoutubeScreenHeight - self.topPlayerView.frame.origin.y - self.topPlayerView.frame.height)
                 }, completion: {completed in
